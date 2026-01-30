b6ae52fd2e98c790a67833eca5283ad4cdd15900
Karun-Kumar-Undrajavarapu <105149067+Karun-Kumar-Undrajavarapu@users.noreply.github.com>
2026-01-30 13:02:42 +0530

    Refactor configure method parameter name in Java-Application/edit/main/src/main/java/com/example/UserManagementApplication.java

--- src/main/java/com/example/UserManagementApplication.java
+++ src/main/java/com/example/UserManagementApplication.java
@@ -9,8 +9,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 public class UserManagementApplication extends SpringBootServletInitializer {
 
     @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(UserManagementApplication.class);
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(UserManagementApplication.class);
     }
 
     public static void main(String[] args) {
